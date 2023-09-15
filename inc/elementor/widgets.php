f387c140634b1a137dbcbc39e71ad7ca883560c9
hmbashar <hmbashar@gmail.com>
2023-09-16 01:01:37 +0600

    ajax added

--- inc/elementor/widgets.php
+++ inc/elementor/widgets.php
@@ -1,31 +1,88 @@
-<?php 
-class CBEM_Sponsors extends \Elementor\Widget_Base {
+<?php
+class CBEM_Sponsors extends \Elementor\Widget_Base
+{
 
-	public function get_name() {
-		return 'hello_world_widget_1';
-	}
+    public function get_name()
+    {
+        return 'cbem_sponsors';
+    }
 
-	public function get_title() {
-		return esc_html__( 'Hello World 1', 'elementor-addon' );
-	}
+    public function get_title()
+    {
+        return esc_html__('CBEM Sponsors', 'elementor-addon');
+    }
 
-	public function get_icon() {
-		return 'eicon-code';
-	}
+    public function get_icon()
+    {
+        return 'eicon-code';
+    }
 
-	public function get_categories() {
-		return [ 'basic' ];
-	}
+    public function get_categories()
+    {
+        return ['basic'];
+    }
 
-	public function get_keywords() {
-		return [ 'hello', 'world' ];
-	}
+    public function get_keywords()
+    {
+        return ['hello', 'world'];
+    }
 
-	protected function render() {
-		?>
+    protected function render()
+    {
+        $sponsors = new WP_Query(
+            array(
+                'post_type' => 'sponsor',
+                'posts_per_page' => -1,
+                // 'order'     => 'ASC', 
+                // 'orderby'   => 'menu_order'
+            )
+        );
+        ?>
 
-		<p> Hello World </p>
+        <script>
+            jQuery(document).ready(function($) {
+                $('.cbem-sponsor-logo-wrapper').on('click', function() {
+                    //ajax call
+                    $.ajax({
+                        url:"<?php echo admin_url('admin-ajax.php'); ?>",
+                        type: "POST",
+                        data: {
+                            action: "cbem_get_sponsor_details",
+                            id:$(this).data("id")
+                        },
+                        success: function(response) {
+                            console.log(response);
+                        }
+                    });
+                });
+            });
+        </script>
 
-		<?php
-	}
+
+        <div class="flex mx-4 justify-center">
+
+            <?php
+            if ($sponsors->have_posts()):
+                while ($sponsors->have_posts()):
+                    $sponsors->the_post();
+
+                    ?>
+
+                    <div class="px-4 w-1/4">
+                        <div class="cbem-sponsor-logo-wrapper" data-id="<?php echo get_the_ID(); ?>">
+                            <div class="cbem-sponsor-logo">
+                                <?php the_post_thumbnail('large'); ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <?php
+
+                endwhile;
+            endif;
+            wp_reset_query();
+            ?>
+        </div>
+    <?php
+    }
 }
--- inc/functions.php
+++ inc/functions.php
@@ -4,10 +4,31 @@
 
 function cbem_elementor_widgets($widgets_manager) {
 
-    include_once('inc/elementor/widgets.php');
+    include_once('elementor/widgets.php');
 
     $widgets_manager->register(new CBEM_Sponsors());
 
 }
 
 add_action('elementor/widgets/register', 'cbem_elementor_widgets');
+
+
+//ajax enqueue for sponsor ajax action 
+
+add_action('wp_ajax_cbem_get_sponsor_details', 'cbem_get_sponsor_details');
+add_action('WP_ajax_nopriv_cbem_get_sponsor_details', 'cbem_get_sponsor_details');
+
+function cbem_get_sponsor_details() {
+    $id = $_POST['id'];
+    $post = get_post($id);
+
+
+
+    $response = array(
+        'post' => $post,
+    );
+
+
+    echo json_encode($response);
+    die();
+}
